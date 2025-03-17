bbdb5833ea2567d5146a64cc1a12208267bedb0c
Your Name <you@example.com>
2025-03-17 19:15:09 -0400

    update get-help content

--- Invoke-Collection.ps1
+++ Invoke-Collection.ps1
@@ -2,14 +2,28 @@ using module .\Modules\DataPoint\DataPoint.psm1
 function Invoke-Collection {
 <#
 .SYNOPSIS
-    Invoke-Collection is a mass survey tool that facilitates the rapid collection 
+    Invoke-Collection is one part of mass survey tool that facilitates the rapid collection 
     of curated data points from Microsoft Windows hosts.
 
 .DESCRIPTION
-    
-.PARAMETER ComputerName
+.PARAMETER LocalCollection
+    Use this to run the collector against the local machine only.
+.PARAMETER RemoteCollection
+    Use this to run the collecter against one or more remote hosts.
+.PARAMETER CollectAll
+    Use this to collect all datapoints.
+.PARAMETER Collect
+    Use this to collect specific datapoints.
+.PARAMETER CollectCategory
+    Use this to collect all datapoints that can help hunt for a specific technique category such as persistence, lateral movement, etc.
+.PARAMETER OutFolder
+    The parent directory that you want the collection stored under.
+.PARAMETER OutputFormat
+    Currently supports output as csv or json.
 .EXAMPLE
+    Invoke-Collection -LocalCollection -CollectAll -OutFolder C:\Meta-Blue\ -OutputFormat json
 .EXAMPLE
+    Invoke-Collection -RemoteCollection -ComputerSet ActiveDirectory -Collect Processes -OutFolder C:\Meta-Blue\ -OutputFormat json
 .INPUTS
 .OUTPUTS
     An ungodly amount of CSVs in your specified directory.
@@ -52,7 +66,7 @@ function Invoke-Collection {
         [string]$OutFolder = "C:\Meta-Blue",
 
         [ValidateSet('csv','json')]
-        [string]$OutputFormat
+        [string]$OutputFormat = 'csv'
     )
     BEGIN {
         $timestamp = (get-date).Tostring("yyyy_MM_dd_hh_mm_ss")
@@ -96,6 +110,7 @@ function Invoke-Collection {
                 This is the action that the following event that is registered takes 
             #>
             $action =  {
+                
                 $Task = $Sender.name;
                 if($Sender.state -eq "Completed"){
     
